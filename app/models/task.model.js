62f6ae7a67926399e8c7481fc338167aad6d6c7f
osvaldosilitonga <osvaldosilitonga@gmail.com>
2024-07-30 13:57:33 +0700

    rafactor

--- app/models/task.model.js
+++ app/models/task.model.js
@@ -3,22 +3,18 @@ const mongoose = require('mongoose')
 const TaskSchema = mongoose.Schema({
     title: {
         type: String,
-        required: [true, "Please enter task title"]
     },
     description: {
         type: String,
-        required: [true, "Please enter task description"],
     },
     is_complete: {
         type: Boolean
     },
     start_time: {
         type: Date,
-        required: [true, "Please enter start time"],
     },
     end_time: {
         type: Date,
-        required: [true, "Please enter end time"],
     },
 }, { timestamps: true })
 
